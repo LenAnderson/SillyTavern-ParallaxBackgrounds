ac6c4460cd00bcf54835078ec97a6907ee4d59c1
LenAnderson <Anderson.Len@outlook.com>
2024-06-04 19:22:03 -0400

    add desert

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 Just a small demo.
 
-Endless train ride through a forest at night.
+Endless train ride through a forest or desert at night.
+
+Use the following slash commands to switch between the two backgrounds.
+
+```
+// forest |
+/parallax 0
+```
+
+```
+// desert |
+/parallax 1
+```
 
 ![](README/para-02.jpg)
--- index.js
+++ index.js
@@ -1,32 +1,91 @@
-const layers = [
-    '00_Train',
-    '01_Mist',
-    '02_Bushes',
-    '03_Particles',
-    '04_Forest',
-    '05_Particles',
-    '06_Forest',
-    '07_Forest',
-    '08_Forest',
-    '09_Forest',
-    '10_Sky',
+import { SlashCommand } from "../../../slash-commands/SlashCommand.js";
+import { ARGUMENT_TYPE, SlashCommandArgument } from "../../../slash-commands/SlashCommandArgument.js";
+import { SlashCommandParser } from "../../../slash-commands/SlashCommandParser.js";
+
+const bgs = [
+    {
+        dur: 10000,
+        excl: [],
+        layers: [
+            'parallax-01/00_Train',
+            'parallax-01/01_Mist',
+            'parallax-01/02_Bushes',
+            'parallax-01/03_Particles',
+            'parallax-01/04_Forest',
+            'parallax-01/05_Particles',
+            'parallax-01/06_Forest',
+            'parallax-01/07_Forest',
+            'parallax-01/08_Forest',
+            'parallax-01/09_Forest',
+            'parallax-01/10_Sky',
+        ],
+    },
+    {
+        dur: 13000,
+        excl: [
+            'parallax-02/03_Moon',
+            'parallax-02/02_Stars',
+            'parallax-02/01_Sky',
+        ],
+        layers: [
+            'parallax-02/00_Train',
+            'parallax-02/12_Desert',
+            'parallax-02/11_Mountains',
+            'parallax-02/10_Cloudsmall',
+            'parallax-02/09_Cloudsmall',
+            'parallax-02/08_Cloud',
+            'parallax-02/07_Cloud',
+            'parallax-02/06_Cloud',
+            'parallax-02/05_Cloud',
+            'parallax-02/04_Cloud',
+            'parallax-02/03_Moon',
+            'parallax-02/02_Stars',
+            'parallax-02/01_Sky',
+        ],
+    },
 ];
+
 
 const wrapper = document.createElement('div'); {
     wrapper.classList.add('stpb--wrapper');
-    let dur = 10000;
+    wrapper.innerHTML = '';
+    document.body.append(wrapper);
+}
+
+const makeBg = ()=>{
+    let bg = bgs[JSON.parse(localStorage.getItem('stpb--bg') ?? '0')];
+    let layers = bg.layers;
+    let dur = bg.dur ?? 10000;
     for (const layer of layers.toReversed()) {
         const l = document.createElement('div'); {
             l.classList.add('stpb--layer');
             const img = document.createElement('div'); {
                 img.classList.add('stpb--img');
-                img.style.backgroundImage = `url(/scripts/extensions/third-party/SillyTavern-ParallaxBackgrounds/parallax-01/${layer}.png)`;
+                img.style.backgroundImage = `url(/scripts/extensions/third-party/SillyTavern-ParallaxBackgrounds/${layer}.png)`;
                 img.style.setProperty('--stpb--duration', `${dur}ms`);
+                if (bg.excl.includes(layer)) {
+                    img.style.animation = 'unset';
+                }
                 l.append(img);
             }
             wrapper.append(l);
         }
         dur = Math.round(dur * 0.9);
     }
-    document.body.append(wrapper);
-}
+};
+makeBg();
+
+SlashCommandParser.addCommandObject(SlashCommand.fromProps({ name: 'parallax',
+    callback: (args, value)=>{
+        const num = Number(value);
+        localStorage.setItem('stpb--bg', JSON.stringify(num));
+        makeBg();
+    },
+    unnamedArgumentList: [
+        SlashCommandArgument.fromProps({ description: 'which background to show',
+            typeList: [ARGUMENT_TYPE.NUMBER],
+            enumList: ['0', '1'],
+        }),
+    ],
+    helpString: 'Set which parallax background to show (0 or 1)',
+}));
